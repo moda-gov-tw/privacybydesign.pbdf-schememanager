0d2627eeca765c2e6674f69b0409a3dd23bbb716
Toomore Chiang <toomore0929@gmail.com>
2023-04-28 15:03:10 +0800

    Update path
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- _extract.sh
+++ _extract.sh
@@ -1,3 +1,3 @@
 find ./ -name '*.xml' -exec itstool -l 'zh_Hant' -o ./description.pot {} +;
 pogrep --search=notes -e '(\/en)$' -i ./description.pot -o ./description_en.pot
-pot2po -i ./description_en.pot -o ./locals/description_zh_Hant_TW.po
+pot2po -i ./description_en.pot -o ./locals/description_zh_Hant_empty.po
